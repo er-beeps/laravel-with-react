98c5b2a9c6975dc57a6edef83c6dd8d02a1ce4e6
er.beeps <bipinkhatiwada10@gmail.com>
2023-03-24 14:35:56 +0545

    initial commit

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Exception;
+
+class ApiController extends Controller{
+
+    public function register(Request $request)
+    {
+        $data = [
+            'name'=>$request->name,
+            'email'=> $request->email,
+            'password'=>bcrypt($request->password),
+        ];
+
+        try {
+            User::create($data);
+            $status=true;
+            $message='Record successfully created !';
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+            $status=false;
+        }
+
+        return response()->json(['success'=>$status,'message'=>$message]);
+    }
+    public function login(Request $request)
+    {
+        return $request->all();
+    }
+
+}
